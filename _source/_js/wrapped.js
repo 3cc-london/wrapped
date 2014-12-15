00a78549ff0be9582aef1eaedc04e427e60d55f9
stanfst <stanislavlukasov@admins-iMac.local>
2014-12-15 16:32:49 +0000

    Revised version

--- _source/_js/wrapped.js
+++ _source/_js/wrapped.js
@@ -5,13 +5,9 @@ $(document).ready(function () {
 	var closed = true;
 	var selectorclosed = true;
 
-	document.querySelector( "#nav-toggle " ).addEventListener( "click", function() {
-	  this.classList.toggle( "active" );
-	});
+	$( 'a.toggle' ).click(function() {
 
-	$( "#nav-toggle" ).click(function() {
-
-		$( '.cover' ).addClass( 'covertransition' );
+			$( '.cover' ).addClass( 'covertransition' );
 
 		//Close work selector
 			$( '.content-arrow-left' ).css("display", "block");
@@ -24,6 +20,7 @@ $(document).ready(function () {
 			});
 
 			$('.work-curtain ').css("z-index","1");
+			$('.moreworkrow > li').css('visibility','hidden').hide().fadeIn('slow');
 			 selectorclosed = true;
 		//
 
@@ -31,6 +28,8 @@ $(document).ready(function () {
 		{
 			$( '.content-arrow-left' ).css("display", "none");
 			$( '.content-arrow-right' ).css("display", "none");
+			//Burger Animation
+			$(this).toggleClass('active');
 			
 
 			$('.menu-curtain').animate({
@@ -67,6 +66,8 @@ $(document).ready(function () {
 		{
 			$( '.content-arrow-left' ).css("display", "block");
 			$( '.content-arrow-right' ).css("display", "block");
+			//Burger Animation
+			$(this).toggleClass('active');
 
 			$('.menu-curtain').animate({
 				width: '0px'
@@ -74,10 +75,14 @@ $(document).ready(function () {
 					    
 			});
 
-			$('.social-holder > .fa').css('visibility','hidden').hide().fadeIn('slow');
-			$('.menu > li').css('visibility','hidden').hide().fadeIn('slow');
+			setTimeout(function()
+			{
+				$('.menu-curtain ').css("z-index","1");
+			}, 500);
+
+			$('.social-holder > .fa').css('visibility','hidden').hide().fadeOut('slow');
+			$('.menu > li').css('display','none').hide().fadeOut('slow');
 			$('.text').css('visibility','visible').hide().fadeIn('slow');
-			$('.menu-curtain ').css("z-index","1");
 			$('.left-content').css("z-index","3");
 			 closed = true;
 		}
@@ -97,8 +102,15 @@ $(document).ready(function () {
          $(".image-holder").stop().animate({ scrollLeft	:  pos }, 800);
     });
 
+    $(function() {
 
-//Work Selector Animation 
+   		$('.image-holder').mousewheel(function(event, delta) {
+	      this.scrollLeft -= (delta * 1);
+	      event.preventDefault();
+		  });
+	});
+
+//More Work Selector Animation 
 
 	$( '.work-selector' ).click(function() {
 
@@ -129,11 +141,25 @@ $(document).ready(function () {
 			});			
 
 			$('.work-curtain ').css("z-index","3");
+			$('.right-content').css("z-index","4");
+
+			setTimeout(function()
+			    {
+			        var shown = $('.moreworkrow > li:first');
+			        shown.css('visibility','visible').hide().fadeIn('slow').delay(15);
+			        setInterval(function(){
+			            if(shown.next().length){
+			                shown = shown.next();
+			                shown.css('visibility','visible').hide().fadeIn('slow').delay(15);
+			            }
+			        },25);
+			    }, 100);
 
 			  selectorclosed = false;
 		}
 		else
 		{
+			$('.moreworkrow > li').css('visibility','hidden').hide().fadeIn('slow');
 			$( '.content-arrow-left' ).css("display", "block");
 			$( '.content-arrow-right' ).css("display", "block");
 
@@ -143,10 +169,22 @@ $(document).ready(function () {
 					    
 			});
 
-			$('.work-curtain ').css("z-index","1");
+			setTimeout(function()
+			{
+				$('.work-curtain ').css("z-index","1");
+			}, 500);
+
 			 selectorclosed = true;
 		}
 
 	});
 
+//Image replacement 
+	$('.textcover').on('mouseover', function(){
+		console.log(this);
+		$('.a', this).stop().animate({'opacity': '0'}, 300);
+	}).on('mouseout', function(){
+		$('.a', this).stop().animate({'opacity': '1'}, 300);
+	});
+            
 });
