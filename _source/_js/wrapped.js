4b6b6477d2d3d4347c5856b0b57709a296b0d5bf
Stanislav Lukasov <stanfst@gmail.com>
2015-07-08 13:07:44 +0100

    Blog presentation

--- _source/_js/wrapped.js
+++ _source/_js/wrapped.js
@@ -1,24 +1,14 @@
-$(window).load(function() {
-  setTimeout(function()
-  {
-    $('.white-curtain').css({'display': 'none'});
-  }, 800);
-});
-
 $(document).ready(function () {
 
 	$('body').css('visibility','visible').hide().fadeIn('slow');
 	var curtainwidth = $('.text').width();
 	var menucurtainwidth = $('.menu-curtain').width();
-	var leftcontentheight = $('.left-content').height();
+	var leftcontentheight = $('.left-content').height() + 80;
 	var closed = true;
 	var selectorclosed = true;
 	var windowheight = $(window).height();
 	var windowwidth = $(window).width();
-
-	/**
-	*** Changes left menu height
-	*/
+	// $('.menu-curtain').css('max-height', windowheight,'!important');
 
 	function checkGridHeight(){
 		if (windowheight > 725){
@@ -26,29 +16,13 @@ $(document).ready(function () {
 		}
 		else{
 			$('.work-grid').css("overflow","hidden");
-			$('.work-grid').css("height",leftcontentheight);	
-			$('.work-grid').css("min-height","725px");			
+			$('.work-grid').css("height",leftcontentheight);
+			$('.work-grid').css("min-height","725px");
 		}
 	}
 
 	/**
-	*** Lock Grid when menu opens
-	*/
-
-	function lockGrid(){
-		$( '.fullwidth' ).css("position", "fixed");
-	}
-
-	/**
-	*** Unlock Grid when menu opens
-	*/
-
-	function unlockGrid(){
-		$( '.fullwidth' ).css("position", "relative");
-	}
-
-	/**
-	***	Open menu curtain function 	
+	***	Open menu curtain function
 	*/
 
 	function openMenuCurtain(){
@@ -56,7 +30,7 @@ $(document).ready(function () {
 		$( '.content-arrow-right' ).css("display", "none");
 		//Burger Animation
 		$('a.toggle').addClass('active');
-			
+
 		$('.menu-curtain').animate({
 			width: '100%'
 			}, 500, function() {
@@ -64,61 +38,60 @@ $(document).ready(function () {
 			//$('body').toggleClass('overflow');
 			//$('html').toggleClass('overflow');
 
-		});		
+		});
 
-		lockGrid();
 		checkGridHeight();
 
-		$('.text').css('visibility','hidden').hide().fadeOut('slow');
+		$('.text').css('display', 'none');
+		$('.blog').css('display', 'none');
 		$('.menu-curtain ').css("z-index","3");
 		$('.left-content').css("z-index","4");
 
 		setTimeout(function()
 		{
 			var shown = $('.menu > li:first');
-			shown.css('visibility','visible').hide().fadeIn('slow').delay(60);
-			setInterval(function(){
-				if(shown.next().length){
-					shown = shown.next();
-					shown.css('visibility','visible').hide().fadeIn('slow').delay(60);
-				}
-			},80);
+		    shown.css('visibility','visible').stop().hide().fadeIn('slow').delay(60);
+		    setInterval(function(){
+			    if(shown.next().length){
+				    shown = shown.next();
+				    shown.css('visibility','visible').stop().hide().fadeIn('slow').delay(60);
+			    }
+		    },80);
 		}, 500);
 
 		setTimeout(function()
 		{
-			$('.social-holder > a .fa').css('visibility','visible').hide().fadeIn('slow');
+			$('.social-holder > a .fa').css('visibility','visible').stop().hide().fadeIn('slow');
 		}, 900);
 	}
 
 	/**
-	***	Remove menu curtain function 	
+	***	Remove menu curtain function
 	*/
 
 	function removeMenuCurtain(){
 		$('.menu-curtain').animate({
 			width: '0px'
-			}, 500, function() {	
+			}, 500, function() {
 			// Enable vertical scrolling if menu curtain is 100%
-			//$('body').toggleClass('overflow');	 
-			//$('html').toggleClass('overflow');   
+			//$('body').toggleClass('overflow');
+			//$('html').toggleClass('overflow');
 		});
-
-		unlockGrid();
-
-		setTimeout(function()
-		{
-			$('.menu-curtain ').css("z-index","1");
-		}, 500);
 
 		$('.work-grid').css("overflow","visible");
 		$('.work-grid').css("height","100%");
 
 		$('a.toggle').removeClass('active');
-		$('.social-holder > .fa').css('visibility','hidden').hide().fadeIn('slow');
-		$('.menu > li').css('visibility','hidden').hide().fadeIn('slow');
-		$('.text').css('visibility','visible').hide().fadeIn('slow');
+		$('.social-holder > a .fa').css('display', 'none');
+		$('.menu > li').css('display', 'none');
+		$('.text').css('display', 'block');
+		$('.blog').css('display', 'block');
 		$('.left-content').css("z-index","2");
+
+		setTimeout(function()
+		{
+			$('.menu-curtain ').css("z-index","1");
+		}, 500);
 	}
 
 	/**
@@ -131,26 +104,26 @@ $(document).ready(function () {
 		$('.work-curtain').animate({
 			width: '100%'
 			}, 500, function() {
-					    
-		});			
+
+		});
 		$('.work-curtain ').css("z-index","3");
 		$('.right-content').css("z-index","4");
 
 		setTimeout(function()
-		{
-			var shown = $('.moreworkrow > li:first');
-			shown.css('visibility','visible').hide().fadeIn('slow').delay(15);
-			setInterval(function(){
-			if(shown.next().length){
-				shown = shown.next();
-				shown.css('visibility','visible').hide().fadeIn('slow').delay(15);
-			}
-			},25);
-		}, 100);
+	   {
+		   	var shown = $('.moreworkrow > li:first');
+		   	shown.css('visibility','visible').stop().hide().fadeIn('slow').delay(15);
+		    setInterval(function(){
+			    if(shown.next().length){
+				    shown = shown.next();
+				    shown.css('visibility','visible').stop().hide().fadeIn('slow').delay(15);
+			    }
+		   	},25);
+	   }, 100);
 	}
 
 	/**
-	***	Remove more work curtain function 	
+	***	Remove more work curtain function
 	*/
 
 	function removeWorkCurtain(){
@@ -162,7 +135,7 @@ $(document).ready(function () {
 		$('.work-curtain').animate({
 			width: '0px'
 			}, 500, function() {
-					    
+
 		});
 
 		setTimeout(function()
@@ -172,7 +145,7 @@ $(document).ready(function () {
 	}
 
 	/**
-	***	Remove curtains on background click 	
+	***	Remove curtains on background click
 	*/
 
 	$( '.work-content, .cover, .work-grid' ).click(function() {
@@ -186,7 +159,7 @@ $(document).ready(function () {
 	});
 
 	/**
-	***	Remove curtains on cover click 	
+	***	Remove curtains on cover click
 	*/
 
 	$( 'a.toggle' ).click(function() {
@@ -207,7 +180,7 @@ $(document).ready(function () {
 		}
 
 		else
-		{	
+		{
 			// Close menu curtain
 			removeMenuCurtain();
 			closed = true;
@@ -217,7 +190,7 @@ $(document).ready(function () {
 	});
 
 	/**
-	***	 Work Arrows 
+	***	 Work Arrows
 	*/
 
     $('.content-arrow-right').click(function () {
@@ -238,13 +211,13 @@ $(document).ready(function () {
 	});
 
 	/**
-	***	 More Work Selector Animation 
+	***	 More Work Selector Animation
 	*/
 
 	$( '.work-selector' ).click(function() {
 		if (selectorclosed)
 		{
-			// Close Left Menu 
+			// Close Left Menu
 			removeMenuCurtain();
 			closed = true;
 			// Open work curtain
@@ -268,5 +241,5 @@ $(document).ready(function () {
 	});
 
 
-// End of Document.load function            
+// End of Document.load function
 });
